233da58f716fcc330cbffe06e0a79f072ff2f989
derekbrownh <59584928+derekbrownh@users.noreply.github.com>
2020-01-08 16:09:24 -0700

    Updated Firebase Config

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyDO_K_x9tcBZW6PUMyIlQyIqtGubKmkWEE",
+  authDomain: "todo2-5889f.firebaseapp.com",
+  databaseURL: "https://todo2-5889f.firebaseio.com",
+  projectId: "todo2-5889f",
+  storageBucket: "todo2-5889f.appspot.com",
+  messagingSenderId: "257726328410",
+  appId: "1:257726328410:web:05b400c3f28c1548bf7263"
 };
 
 firebase.initializeApp(config);
